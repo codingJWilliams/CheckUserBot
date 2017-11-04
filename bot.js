6eb8bb0891beab20bcb34d980e13ec353bb4e8dc
Jay Williams <course@jaywilliams.me>
2017-11-04 18:19:24 +0000

    Formatted

--- bot.js
+++ bot.js
@@ -5,136 +5,115 @@ var checks = require("./helpers/checks.js");
 var fs = require("fs");
 var userLeaveStorage = require("./helpers/userLeaveStorage");
 var options = {
-  ownerID: "193053876692189184",
-  serverName: "Nightborn Estate",
-  logChannelName: "checkuser-logs"
+    ownerID: "193053876692189184",
+    serverName: "Nightborn Estate",
+    logChannelName: "checkuser-logs"
 }
+
 function clean(text) {
-  if (typeof(text) === "string") { return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)) }
-  else { return text };
+    if (typeof(text) === "string") { return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)) } else { return text };
 }
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
+
 function doAutoban(arg) {
-  var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"))
-  flags.autoban = arg;
-  fs.writeFileSync("./storage/flags.json", JSON.stringify(flags))
+    var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"))
+    flags.autoban = arg;
+    fs.writeFileSync("./storage/flags.json", JSON.stringify(flags))
 }
+
 function getAutoban() {
-  var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"))
-  return flags.autoban
+    var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"))
+    return flags.autoban
 }
 // Eval command. Only for Void
 client.on("message", message => {
-  const args = message.content.split(" ").slice(1);
-  /*if (message.content.startsWith("!ban em")) {
-    var theFile = JSON.parse(fs.readFileSync("ban.json"));
-    (function myLoop (i) {          
-      setTimeout(function () {   
-        try{
-          message.guild.members.find("id", theFile[i - 1]).ban({
-            reason: "PURGE"
-          })    
-        } catch (e) {
-          console.log("N")
-        }
-                 
-         if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
-      }, 500)
-   })(theFile.length);       
-  }*/
-  if (message.content.startsWith("cu.eval")) {
-    if(message.author.id !== options.ownerID) return;
-    try {
-      let evaled = eval(args.join(" "));
-      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-  if (message.content.startsWith("cu.silenteval")) {
-    if(message.author.id !== options.ownerID) return;
-    try {
-      let evaled = eval(args.join(" "));
-      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-      //message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      //message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-  if (message.content.startsWith("cu.vpsinfo")) {
-    if(message.author.id !== options.ownerID) return;
-    var si = require("systeminformation");
-    si.mem().then( (mInfo) => {
-      si.cpu().then( cInfo => {
-        message.reply({
-          embed: {
-            title: "VPS Data",
-            fields: [
-              {
-                "name": "Memory",
-                "value": ` Total memory: ${mInfo.total / 1000000}mb.
-Free memory: ${mInfo.free / 1000000}mb.
-                `
-              },
-              {
-                "name": "CPU Info",
-                "value": `Type: ${ cInfo.manufacturer + cInfo.brand + cInfo.model + cInfo.revision}
-Cores: ${cInfo.cores}
-Max Speed: ${cInfo.speedmax}
-Current speed: ${cInfo.speed}`
-              }
-            ]
+    const args = message.content.split(" ").slice(1);
+    /*if (message.content.startsWith("!ban em")) {
+      var theFile = JSON.parse(fs.readFileSync("ban.json"));
+      (function myLoop (i) {          
+        setTimeout(function () {   
+          try{
+            message.guild.members.find("id", theFile[i - 1]).ban({
+              reason: "PURGE"
+            })    
+          } catch (e) {
+            console.log("N")
           }
-        })
-      } ) 
-    } )
-    
-  }
-
+                   
+           if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
+        }, 500)
+     })(theFile.length);       
+    }*/
+    if (message.content.startsWith("cu.eval")) {
+        if (message.author.id !== options.ownerID) return;
+        try {
+            let evaled = eval(args.join(" "));
+            if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+            message.channel.send(clean(evaled), { code: "xl" });
+        } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
+    }
+    if (message.content.startsWith("cu.silenteval")) {
+        if (message.author.id !== options.ownerID) return;
+        try {
+            let evaled = eval(args.join(" "));
+            if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+            //message.channel.send(clean(evaled), {code:"xl"});
+        } catch (err) {
+            //message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
+    }
 });
 client.on("guildMemberRemove", member => {
-  userLeaveStorage.userLeft(member);
+    userLeaveStorage.userLeft(member);
 })
 client.on('guildMemberAdd', member => {
-  if (member.guild.name === options.serverName) {
-    checks.check(member).then( results => {
-      if (results.pass) {
-        // If user has passed test, send a positive message
-        member.guild.channels.find("name", options.logChannelName).send({
-          "embed": {
-            "title": `${member.user.username}#${member.user.discriminator} joined the server`,
-            "description": "All the checks passed. **itz kk guyz :ok_hand:**",
-            "color": 524032,
-            "footer": {
-              "text": `${results.passed.length}/${results.passed.length} checks passed`
-            },
-            "author": {
-              "name": `New Member`
-            }
-          }
-        });
-      } else { // User failed one or more tests
-        console.log(member.user.username)
-        member.guild.channels.find("name", options.logChannelName)
-        .send(
-          checks.pretty(results, member.user.username + "#" + member.user.discriminator)
-        );
-        var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"))
-        if (flags.autoban) {
-          member.ban({
-            reason: "CheckUser Failed",
-            days: 1
-          })
+    if (member.guild.name === options.serverName) {
+        var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"));
+        if (!flags.allowNewMembers) {
+            member.createDM().then(d => {
+                d.send("An admin has disabled joining this server. Please try again later");
+                d.kick("Joins Disabled")
+            })
         }
-        
-        // :-)
-      }
-    } )
-  }
+        checks.check(member).then(results => {
+            if (results.pass) {
+                // If user has passed test, send a positive message
+                member.guild.channels.find("name", options.logChannelName).send({
+                    "embed": {
+                        "title": `${member.user.username}#${member.user.discriminator} joined the server`,
+                        "description": "All the checks passed. **itz kk guyz :ok_hand:**",
+                        "color": 524032,
+                        "footer": {
+                            "text": `${results.passed.length}/${results.passed.length} checks passed`
+                        },
+                        "author": {
+                            "name": `New Member`
+                        }
+                    }
+                });
+            } else { // User failed one or more tests
+                console.log(member.user.username)
+                member.guild.channels.find("name", options.logChannelName)
+                    .send(
+                        checks.pretty(results, member.user.username + "#" + member.user.discriminator)
+                    );
+                var flags = JSON.parse(require("fs").readFileSync("./storage/flags.json"));
+                if (flags.autoban) {
+                    member.ban({
+                        reason: "CheckUser Failed",
+                        days: 1
+                    })
+                }
+
+                // :-)
+            }
+        })
+    }
 });
 
 client.login(token);
